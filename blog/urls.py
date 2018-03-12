8d018fe505e2ad4abc570e1f15374b5716166e88
92hoy <ho_9209@naver.com>
2018-03-12 16:46:06 +0900

    blog-select, happy-delete test-

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^blog_read$', views.blog_read, name='blog_read'),
-] 
+    url(r'^blog_read_view/(?P<page>[0-9]{1,3})$', views.blog_read_view, name='blog_read_view'),
+
+    url(r'^happy$', views.happy, name='happy'),
+    url(r'^save$', views.happy_save, name='happy_save'),
+    url(r'^delete', views.happy_delete, name='happy_delete'),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -30,8 +30,10 @@ def index(request):
             '''.format(title=title, content=content, name=name, pw=pw)
             cur.execute(query)
 
-            rows = cur.fetchall()
-            print rows
+            print query
+
+            #rows = cur.fetchall()
+            #print rows
 
         return JsonResponse({'return':'success'})
 
@@ -41,5 +43,118 @@ def index(request):
 
 def blog_read(request):
 
-    return render(request,'blog_read.html',{})
+    with connections['default'].cursor() as cur:
+        query = '''
 
+
+            SELECT no, title, content, name,
+            DATE_FORMAT(resist_date, "%Y-%m-%d %H:%m:%s"),
+            DATE_FORMAT(modify_date, "%Y-%m-%d %H:%m:%s")
+            FROM one
+            order by resist_date asc
+        '''
+        cur.execute(query)
+
+        rows = cur.fetchall()
+        print rows
+
+    context = {}
+
+    # context['key'] = 'value'
+    # context['hello'] = 333
+    # context['test'] = [1,2,3]
+    context['rows'] = rows
+
+    return render(request,'blog_read.html',context)
+
+def blog_read_view(request, page):
+
+    print "page = ", page
+
+    with connections['default'].cursor() as cur:
+        query = '''
+
+
+            SELECT no, title, content, name,
+            DATE_FORMAT(resist_date, "%Y-%m-%d %H:%m:%s"),
+            DATE_FORMAT(modify_date, "%Y-%m-%d %H:%m:%s")
+            FROM one
+            where no = {page}
+        '''.format(page=page)
+
+        print query
+
+        cur.execute(query)
+
+        rows = cur.fetchall()
+
+
+    context={}
+
+    context['read'] = rows[0]
+    return render(request,'blog_read_view.html',context)
+
+def happy(request):
+
+    with connections['default'].cursor() as cur:
+        query = '''
+            select title, no
+            from one
+            order by resist_date asc
+        '''
+        cur.execute(query)
+
+        print query
+
+        rows = cur.fetchall()
+
+    context = {}
+    context['rows'] = rows
+    return render(request,'happy.html',context)
+
+def happy_save(request):
+
+    input = request.GET.get('input')
+
+    with connections['default'].cursor() as cur:
+            query = '''
+                insert into one(title, content, name, pw)
+                values('{title}','99999','99999','99999')
+            '''.format(title=input)
+            cur.execute(query)
+
+            print query
+
+    with connections['default'].cursor() as cur:
+            query = '''
+                select title, no
+                from one
+                order by resist_date desc
+            '''
+            cur.execute(query)
+
+            print query
+
+            rows = cur.fetchall()
+            title = rows[0][0]
+            no = rows[0][1]
+
+    return JsonResponse({'return':'success', 'title':title, 'no':no})
+
+def happy_delete(request):
+
+    no = request.GET.get('no')
+
+    print no
+
+    with connections['default'].cursor() as cur:
+            query = '''
+                delete from one
+                where no = {no}
+            '''.format(no=no)
+            cur.execute(query)
+
+            print query
+
+
+    return JsonResponse({'return':'success', 'no':no})
